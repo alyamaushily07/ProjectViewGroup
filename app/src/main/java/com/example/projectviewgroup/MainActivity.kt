bcecb0e208f55b61dd02c558e6d11d46d7900899
Alya <alyamaushily77@gmail.com>
2023-02-02 13:36:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectviewgroup/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectviewgroup
+
+import android.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
